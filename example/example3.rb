b9ccbaa255617fa2bd4bdc572070f7fa14d64430
John Cupitt <jcupitt@gmail.com>
2015-10-20 17:14:51 +0100

    simpler example

--- example/example3.rb
+++ example/example3.rb
@@ -2,21 +2,19 @@
 
 require 'vips8'
 
-puts ""
-puts "starting up:"
 # this makes vips keep a list of all active objects 
-Vips::leak_set true
+#Vips::leak_set true
 
 # disable the operation cache
-Vips::cache_set_max 0
+#Vips::cache_set_max 0
 
-ARGV.each do |filename|
-    im = Vips::Image.new_from_file filename, :access => :sequential
+# any image is fine
+filename = "/home/john/pics/k2.jpg"
+
+100.times do |i|
+    puts "loop #{i} ..."
+    im = Vips::Image.new_from_file filename
     im = im.embed 100, 100, 3000, 3000, :extend => :mirror
     im.write_to_file "x.v"
 end
 
-puts ""
-puts "shutting down:"
-im = nil
-GC.start
